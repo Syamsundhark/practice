7775626d893d17316c40c67d4af715091dc2bf39
SyamSundharKudumala <syam.sundharkudumala@diggibyte.com>
2023-07-31 19:19:18 +0530

    This is a util file containing code for oops concepts

---
+++ src/practice1/util.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder, balance=0):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.balance}")
+        else:
+            print("Invalid deposit amount. Please deposit a positive value.")
+
+    def withdraw(self, amount):
+        if (0 < amount <= self.balance):
+            self.balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.balance}")
+        else:
+            print("Insufficient funds or invalid withdrawal amount.")
+
+    def get_balance(self):
+        return self.balance
+
+

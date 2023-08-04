36f3eb93991d3ba4ddef26b4291af397515c8c55
SyamSundharKudumala <syam.sundharkudumala@diggibyte.com>
2023-08-04 19:35:01 +0530

    This is a driver file containing code for Assignment11.

---
+++ src/Assignment11/driver.py
@@ -0,0 +1,11 @@
+from src.Assignment11.util import *
+
+# n = int(input())
+# emails = [input().strip() for _ in range(n)]
+#
+# valid_emails = list(filter(validemail, emails))
+# valid_emails.sort()
+#
+# print(valid_emails)
+
+validateemail()
